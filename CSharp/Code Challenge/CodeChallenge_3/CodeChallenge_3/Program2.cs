80f90f4989323df03a7c8d9210e59e13fb79f6b2
Akhilesh Singh <akhileshsing@infinite.com>
2025-07-09 11:36:49 +0530

    updated code

--- CSharp/Code Challenge/CodeChallenge_3/CodeChallenge_3/Program2.cs
+++ CSharp/Code Challenge/CodeChallenge_3/CodeChallenge_3/Program2.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 
 namespace CodeChallenge_3
-{ 
+{
     class Box
     {
         public int Length { get; set; }
         public int Breadth { get; set; }
+
         public Box(int length, int breadth)
         {
             Length = length;
             Breadth = breadth;
         }
+
         public static Box operator +(Box b1, Box b2)
         {
             return new Box(b1.Length + b2.Length, b1.Breadth + b2.Breadth);
         }
+
         public void Display()
         {
             Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
@@ -25,27 +28,35 @@ namespace CodeChallenge_3
     {
         static void Main(string[] args)
         {
-            int length, breadth;
-            Console.WriteLine("Enter the Lenght of Box 1");
-            length = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Enter the Length of Box 1:");
+                int length1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the Breadth of Box 1");
-            breadth = Convert.ToInt32(Console.ReadLine());
-            Box box1 = new Box(length, breadth);
+                Console.WriteLine("Enter the Breadth of Box 1:");
+                int breadth1 = Convert.ToInt32(Console.ReadLine());
+                Box box1 = new Box(length1, breadth1);
 
-            Console.WriteLine("Enter the Lenght of Box 2");
-            length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Length of Box 2:");
+                int length2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the Breadth of Box 1");
-            breadth = Convert.ToInt32(Console.ReadLine());
-           
-            Box box2 = new Box(length, breadth);
+                Console.WriteLine("Enter the Breadth of Box 2:");
+                int breadth2 = Convert.ToInt32(Console.ReadLine());
+                Box box2 = new Box(length2, breadth2);
 
-    
-            Box box3 = box1 + box2;
+                Box box3 = box1 + box2;
 
-            Console.WriteLine("Details of the resulting box after addition:");
-            box3.Display();
+                Console.WriteLine("Details of the resulting box after addition:");
+                box3.Display();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter integer values only.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+            }
 
             Console.Read();
         }
